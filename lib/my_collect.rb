d866ceab178365bc1766a5a65a9c7125e14bfb89
langan.kg Langan <langan.kg@gmail.com>
2021-03-22 15:42:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,14 +6,7 @@ def my_collect(array)
         collection << yield(array[i])
         i += 1
     end
+    collection
 end
 
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-end
